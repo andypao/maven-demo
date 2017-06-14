d95b9461a03856d91cc964793a3cb896b2c8a9b9
andyp <andypaolini@gmail.com>
2017-06-13 21:41:47 -0400

    New Changes

--- src/test/java/testpackage/mainTest.java
+++ src/test/java/testpackage/mainTest.java
@@ -1,45 +1,27 @@
 package testpackage;
 import java.io.IOException;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import ManageWebDriver.CustomDriver;
 import org.junit.Assert;
 
-public class mainTest {
-
-	public static WebDriver driver;
-	
+public class mainTest extends CustomDriver {
 	
 	@Test	
-	public void sampleRun() throws IOException{
-		driver.get("https://es.wikipedia.org/wiki/Wikipedia:Portada");
-		driver.findElement(By.id("searchInput")).sendKeys("test");
-		driver.findElement(By.id("searchButton")).click();
-		String text  = driver.findElement(By.xpath("//*[@id='mw-content-text']/ul[1]/li/b/a[contains(.,'examen')]")).getText();
-		Assert.assertEquals("test string is displayed properly", text, "exame");
-	}
-
-	@BeforeTest
-	public static void initializeWebDriver() throws IOException
-	{
-	    System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("ignore-certifcate-errors");
-        chromeOptions.addArguments("test-type");
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-        driver = new ChromeDriver(chromeOptions);
+	public void sampleRunToFail() throws IOException{
+		navigateTo("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+		sendText("//*[@name='search']","test");
+		click("//*[@id='searchButton']");
+		String text = getText("//*[@id='mw-content-text']/ul[1]/li/b/a[contains(.,'examen')]");
+		Assert.assertEquals("test string is displayed properly", text, "bad input");
 	}
 	
-	@AfterTest
-	public static void closeDriver(){
-		driver.quit();
+	@Test	
+	public void sampleRunToPass() throws IOException{
+		navigateTo("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+		sendText("//*[@name='search']","test");
+		click("//*[@id='searchButton']");
+		String text = getText("//*[@id='mw-content-text']/ul[1]/li/b/a[contains(.,'examen')]");
+		Assert.assertEquals("test string is displayed properly", text, "examen");
 	}
 	
 }
